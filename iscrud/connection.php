0086e7ec830d8cf390468b69a6482500882154a0
AIsaac10 <168681995+AIsaac10@users.noreply.github.com>
2025-05-15 20:30:36 -0300

    Delete iscrud directory

--- iscrud/connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try {
-    $connection = new PDO("mysql:host=localhost;dbname=PlantGestor;charset=utf8mb4", "root", "");
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-}catch (PDOException $e) {
-    echo "Erro na conexão:". $e->getmessage();
-}
-?>
